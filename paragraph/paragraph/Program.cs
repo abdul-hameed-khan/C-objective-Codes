41ef2a369acb1fa8baa8e99ec2eb48b4f81f0480
Abdul Hameed Khan <abdulhameed314khan@gmail.com>
2022-08-13 09:16:39 +0500

    Add files via upload

---
+++ paragraph/paragraph/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace paragraph
+{
+    class Program
+    {
+        public int Letters(string s)
+        {
+            int count=s.Length ;
+            foreach(int i in s)
+            {
+               
+            if(i == ' ' || i == '.' || i == '?' || i == '!')
+                {
+                    count--;
+                }
+               
+            }
+            return count;
+
+        }
+        public int Words(string s)
+        {
+            
+            int word = 0;
+            for (int count = 0; count < (s.Length); count++)
+            {
+                if (s[count] == ' ' || s[count] == '\t' || s[count] == ',' || s[count] == '.' || s[count] == '?' || s[count] == ':' || s[count] == ';' || s[count] == '!')
+                {
+                    word++;
+                }
+                
+            }
+            return word;
+
+        }
+        public int Sentences(string s)
+        {
+            
+            int sen = 0;
+            for (int count = 0 ; count < (s.Length); count++)
+            {
+                if (s[count] == '.' || s[count] == '?' || s[count] == '!')
+                {
+                    sen++;
+                }
+                
+            }
+            return sen;
+        }
+    
+        static void Main(string[] args)
+        {
+            string str;
+            Console.WriteLine("Enter a paragraph : ");
+            str = Console.ReadLine();
+            Program p = new Program();
+            Console.WriteLine($"Sentences : {p.Sentences(str)}");
+            Console.WriteLine($"Words : {p.Words(str)}");
+            Console.WriteLine($"Letters : {p.Letters(str)}");
+
+            
+        }
+    }
+}
