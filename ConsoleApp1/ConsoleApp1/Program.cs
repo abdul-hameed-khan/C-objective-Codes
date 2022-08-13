ea6615f6f83ef0f79d901a53e28772bef5ab81ac
Abdul Hameed Khan <abdulhameed314khan@gmail.com>
2022-08-13 09:15:52 +0500

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World\f");
+            Console.WriteLine("ABDUL HAMEED KHAN");
+            Console.WriteLine("please enter\f your age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("your age is {0}",age);
+            
+            var arr = new int[5] { 1,2,3,4,5 };
+            Console.WriteLine(Add(arr,2, 6));
+        }
+
+        static int Add(int[] ar,int a, int b)
+        {
+            var total = 0;
+            for( int i = 0; i<5; i++)
+            {
+                total = total + ar[i];
+            }
+            Console.WriteLine(a + b);
+            return total;
+        }
+    }
+}
