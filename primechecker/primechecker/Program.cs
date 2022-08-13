91717e2565a01ac7b93a83673b96671a7f2dab3c
Abdul Hameed Khan <abdulhameed314khan@gmail.com>
2022-08-13 09:17:20 +0500

    Add files via upload

---
+++ primechecker/primechecker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace prmechecking
+{
+    class Program
+    {
+        public static bool IsPrime(int n, int i)
+        {
+            if (n == 0 || n == 1)
+                return false;
+            if (n % i == 0)
+                return false;
+            if (i*i > n)
+                return true;
+
+            return IsPrime(n, i + 1);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(IsPrime(num, 2));
+        }
+    }
+
+
+}
